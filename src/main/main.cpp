e1e7bb7df475aa2e16d5dfeaea10e50f7957ef0e
mooninsky1 <moon_in_sky@sina.cn>
2017-08-29 10:59:01 +0800

    code

---
+++ src/main/main.cpp
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int main(int argc, char* argv[])
+{	
+	//test mongo
+	//CMongoHelp::Test();
+
+	//fcntl test
+	//CFcntl::Test();
+
+	//ioctl test
+	//CIoctl::Test();
+
+	//mmap test
+	//Cmmap::Test();
+
+	//memmove test
+	//CMemMoveTest::Test();
+
+	//fork test
+	//CForkTest::Test();
+
+	//lua test
+	//CLuaTest::Test();
+
+	//python test
+	//CPythonTest::Test();
+
+	//mysql test
+	CMySqlTest::Test();
+	//pause();
+	return 0;
+}
+
---
+++ src/main/main.h
@@ -0,0 +1,25 @@
+#pragma once
+
+//common
+
+#include <set>
+#include <map>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <fstream>
+#include <math.h>
+
+//local
+
+#include "../mongo/mongodbTest.h"
+#include "../fcntl/Fcntl_test.h"
+#include "../ioctl/ioctl_test.h"
+#include "../mmap/mmap_test.h"
+#include "../memmove/memmove_test.h"
+#include "../fork/fork_test.h"
+#include "../lua/lua_test.h"
+#include "../python/python_test.h"
+#include "../mysql/MySqlTest.h"
+using namespace std;
+
